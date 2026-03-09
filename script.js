73cbe21f84a332f7ca3ea32165f6217b5edfc36b
56quique <enriqueklenser241@gmail.com>
2026-03-09 20:46:06 -0300

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,23 +1,87 @@
-const elementos = document.querySelectorAll(".aparecer");
+const cursos = document.querySelectorAll(".curso:not(.no-disponible)");
 
-function mostrarElementos(){
+const modal = document.getElementById("modal");
 
-const trigger = window.innerHeight * 0.85;
+const titulo = document.getElementById("tituloCurso");
+const contenido = document.getElementById("contenidoCurso");
 
-elementos.forEach(el => {
+const cerrar = document.querySelector(".cerrar");
 
-const top = el.getBoundingClientRect().top;
+cursos.forEach(curso => {
 
-if(top < trigger){
+curso.addEventListener("click", () => {
 
-el.classList.add("visible");
+const tipo = curso.dataset.curso;
+
+if(tipo === "electricidad"){
+
+titulo.innerText="Electricidad Industrial";
+
+contenido.innerHTML=`
+<h3>Objetivo</h3>
+<p>Comprender la distribución eléctrica industrial.</p>
+
+<h3>Unidades</h3>
+<ul>
+<li>Esquema de instalación eléctrica</li>
+<li>Cargas industriales</li>
+<li>CCM y protecciones</li>
+<li>Seguridad eléctrica</li>
+<li>Diagnóstico de fallas</li>
+</ul>
+
+<p><b>Duración:</b> 4 horas</p>
+`;
 
 }
+
+if(tipo==="neumatica"){
+
+titulo.innerText="Neumática Industrial";
+
+contenido.innerHTML=`
+<ul>
+<li>Producción de aire comprimido</li>
+<li>Preparación del aire</li>
+<li>Válvulas neumáticas</li>
+<li>Cilindros</li>
+</ul>
+`;
+
+}
+
+if(tipo==="electronica"){
+
+titulo.innerText="Electrónica Industrial";
+
+contenido.innerHTML=`
+<ul>
+<li>Componentes electrónicos</li>
+<li>Electrónica de potencia</li>
+<li>Diagnóstico de placas</li>
+</ul>
+`;
+
+}
+
+modal.style.display="flex";
 
 });
 
+});
+
+cerrar.onclick=function(){
+
+modal.style.display="none";
+
 }
 
-window.addEventListener("scroll", mostrarElementos);
+window.onclick=function(e){
 
-mostrarElementos();
+if(e.target==modal){
+
+modal.style.display="none";
+
+}
+
+}

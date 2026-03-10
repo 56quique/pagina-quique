c0e9b424a4b13068f8dec124fd5083f420639837
56quique <enriqueklenser241@gmail.com>
2026-03-09 22:12:20 -0300

    Actualizar script.js

--- script.js
+++ script.js
@@ -1,82 +1,39 @@
-const cursos = document.querySelectorAll(".curso:not(.no-disponible)");
+const tarjetas=document.querySelectorAll(".tarjeta");
 
-const modal = document.getElementById("modal");
+const modal=document.getElementById("modal");
 
-const titulo = document.getElementById("tituloCurso");
-const contenido = document.getElementById("contenidoCurso");
+const titulo=document.getElementById("tituloCurso");
+const contenido=document.getElementById("contenidoCurso");
 
-const cerrar = document.querySelector(".cerrar");
+const cerrar=document.querySelector(".cerrar");
 
-cursos.forEach(curso => {
+tarjetas.forEach(tarjeta=>{
 
-curso.addEventListener("click", () => {
+tarjeta.addEventListener("click",()=>{
 
-const tipo = curso.dataset.curso;
+let curso=tarjeta.dataset.curso;
 
-if(tipo === "electricidad"){
+if(curso==="electricidad"){
 
 titulo.innerText="Electricidad Industrial";
 
-contenido.innerHTML=`
-<h3>Objetivo</h3>
-<p>Comprender la distribución eléctrica industrial.</p>
-
-<h3>Unidades</h3>
-<ul>
-<li>Esquema de instalación eléctrica</li>
-<li>Cargas industriales</li>
-<li>CCM y protecciones</li>
-<li>Seguridad eléctrica</li>
-<li>Diagnóstico de fallas</li>
-</ul>
-
-<p><b>Duración:</b> 4 horas</p>
-`;
-
-}
-
-if(tipo==="neumatica"){
-
-titulo.innerText="Neumática Industrial";
-
-contenido.innerHTML=`
-<ul>
-<li>Producción de aire comprimido</li>
-<li>Preparación del aire</li>
-<li>Válvulas neumáticas</li>
-<li>Cilindros</li>
-</ul>
-`;
-
-}
-
-if(tipo==="electronica"){
-
-titulo.innerText="Electrónica Industrial";
-
-contenido.innerHTML=`
-<ul>
-<li>Componentes electrónicos</li>
-<li>Electrónica de potencia</li>
-<li>Diagnóstico de placas</li>
-</ul>
-`;
-
-}
+contenido.innerText="Comprender la distribución eléctrica en planta industrial. Incluye tableros, cargas, protecciones y diagnóstico de fallas.";
 
 modal.style.display="flex";
 
-});
+}
 
 });
 
-cerrar.onclick=function(){
+});
+
+cerrar.onclick=()=>{
 
 modal.style.display="none";
 
 }
 
-window.onclick=function(e){
+window.onclick=(e)=>{
 
 if(e.target==modal){
 
